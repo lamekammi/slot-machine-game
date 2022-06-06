59682c55fc35d1a63df17fa3702fb06585a18def
lamekammi <kameronnb@gmail.com>
2022-06-06 14:38:58 -0700

    html, css, and js update

--- js/main.js
+++ js/main.js
@@ -1,9 +1,40 @@
 /*----- constants -----*/
+const items = {
+
+};
 
 /*----- app's state (variables) -----*/
+let winner, squares2, changeMessage;
 
 /*----- cached element references -----*/
+const slotMach = document.getElementById('slot-machine');
+const squares = document.querySelectorAll('.square');
+const leverButt = document.getElementById('lever');
+const resetGameButt = document.getElementById('reset-game');
+const scoreboardEl = document.getElementById('score-board');
 
 /*----- event listeners -----*/
+leverButt.addEventListener('click', leverFunc);
+resetGameButt.addEventListener('click', resetGame);
 
 /*----- functions -----*/
+function leverFunc() {
+    console.log('i am clicked');
+}
+
+function resetGame() {
+    console.log('i am also clicked');
+}
+
+function initGame() {
+
+}
+
+function spin() {
+
+}
+
+function updateScore() {
+
+}
+
