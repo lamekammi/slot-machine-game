1db9cbe1118284ca2db57096a4657b0486ecf7c0
lamekammi <kameronnb@gmail.com>
2022-06-07 11:17:45 -0700

    JS HTML and CSS update

--- js/main.js
+++ js/main.js
@@ -1,10 +1,19 @@
 /*----- constants -----*/
-const items = {
-
-};
+const items = [
+    '🖤',
+    '💛',
+    '💙',
+    '💜',
+    '💚',
+    '🧡',
+    '❤️️',
+    '💗'
+];
 
 /*----- app's state (variables) -----*/
-let winner, squares, changeMessage;
+let winner = null;
+let defualt = '⚫️';
+let squares = [null, null, null];
 
 /*----- cached element references -----*/
 const slotMach = document.getElementById('slot-machine');
@@ -12,17 +21,22 @@ const squaresEl = document.querySelectorAll('.square');
 const leverButt = document.getElementById('lever');
 const resetGameButt = document.getElementById('reset-game');
 const scoreboardEl = document.getElementById('score-board');
+const changeMessage = document.getElementById('message');
+const boxesEl = document.getElementById('box');
 
 /*----- event listeners -----*/
 leverButt.addEventListener('click', leverFunc);
 resetGameButt.addEventListener('click', resetGame);
 
 /*----- functions -----*/
-function leverFunc() {
-    console.log('i am clicked');
+function leverFunc(leverClicked) {
+  
+    console.log('i am clicked')
 }
 
 function resetGame() {
+    winner = null;
+    defualt = '⚫️';
     console.log('i am also clicked');
 }
 
@@ -31,7 +45,7 @@ function initGame() {
 }
 
 function spin() {
-
+    
 }
 
 // do after MVP is done.
